74dfcc1bec96c2b6e630afedf0c0ca0527502d2e
jD91mZM2 <me@krake.one>
2018-02-02 15:33:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# xidlehook
+
+*Because xautolock is annoying to work with.*
+
+xidlehook is a general-purpose replacement for xautolock.  
+It basically just executes a command when the computer has been idle for \_ minutes.
+
+Improvments over xautolock:
+ - As well as a notifier, there is now a canceller, for when the user stops it from locking.
+ - Not specific to locking. Multiple instances can run at the same time.
+
+Missing features:
+ - Magic corners
+ - Killer stuff
+ - Bell, because you should be using --notify
+ - All the instance related stuff (-activate, -restart, -exit, etc)
+
+Planned features:
+ - Optionally prevent locking when an application is fullscreen.
+ - Optional communication using unix sockets to allow activating and deactivating it.
+
+# Example
+
+Here's a lock using i3lock, with screen dim support.
+
+```Bash
+xidlehook \
+  --time 5 \
+  --timer "i3lock" \
+  --notify 10 \
+  --notifier  'xrandr --output "$(xrandr | grep primary | cut -d " " -f 1)" --brightness .1' \
+  --canceller 'xrandr --output "$(xrandr | grep primary | cut -d " " -f 1)" --brightness 1'
+```
---
+++ src/main.rs
@@ -0,0 +1,156 @@
+#[macro_use] extern crate clap;
+extern crate x11;
+
+use clap::{App, Arg};
+use std::os::raw::c_void;
+use std::process::Command;
+use std::time::Duration;
+use std::{ptr, thread};
+use x11::xlib::{Display, XCloseDisplay, XDefaultRootWindow, XFree, XOpenDisplay};
+use x11::xss::{XScreenSaverAllocInfo, XScreenSaverQueryInfo};
+
+struct DeferXClose(*mut Display);
+impl Drop for DeferXClose {
+    fn drop(&mut self) {
+        unsafe { XCloseDisplay(self.0); }
+    }
+}
+struct DeferXFree(*mut c_void);
+impl Drop for DeferXFree {
+    fn drop(&mut self) {
+        unsafe { XFree(self.0); }
+    }
+}
+
+const SCALE: u64 = 60; // Second:minute scale. Can be changed for debugging purposes.
+
+fn main() {
+    let matches = App::new(crate_name!())
+        .author(crate_authors!())
+        .version(crate_version!())
+        .arg(
+            Arg::with_name("print")
+                .help("Prints the idle time to standard output. This is similar to xprintidle.")
+                .long("print")
+        )
+        .arg(
+            Arg::with_name("time")
+                .help("Sets the required amount of idle minutes before executing command")
+                .long("time")
+                .required_unless("print")
+                .requires("timer")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("timer")
+                .help("Sets command to run when timer goes off")
+                .long("timer")
+                .requires("time")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("notify")
+                .help("Runs the command passed by --notifier _ seconds before timer goes off")
+                .long("notify")
+                .requires("notifier")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("notifier")
+                .help("Sets the command to run when notifier goes off (see --notify)")
+                .long("notifier")
+                .requires("notify")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("canceller")
+                .help("Sets the command to run when user prevents the timer after the notifier has already gone off")
+                .long("canceller")
+                .requires("notify")
+                .takes_value(true)
+        )
+        .get_matches();
+
+    let display = unsafe { XOpenDisplay(ptr::null()) };
+    if display.is_null() {
+        eprintln!("failed to open x server");
+        return;
+    }
+    let _cleanup = DeferXClose(display);
+
+    if matches.is_present("print") {
+        if let Ok(idle) = get_idle(display) {
+            println!("{}", idle);
+        }
+        return;
+    }
+
+    let time     = value_t_or_exit!(matches, "time", u32) as u64 * SCALE;
+    let timer    = matches.value_of("timer").unwrap();
+    let notify   = value_t!(matches, "notify", u32).ok().map(|notify| notify as u64);
+    let notifier = matches.value_of("notifier");
+    let canceller = matches.value_of("canceller");
+
+    let mut ran_timer  = false;
+    let mut ran_notify = false;
+
+    let default_delay = Duration::from_secs(SCALE as u64);
+    let mut delay = default_delay;
+
+    let mut time_new = time;
+
+    loop {
+        let idle = match get_idle(display) {
+            Ok(idle) => idle,
+            Err(_) => return
+        };
+
+        let idle = idle / 1000; // Convert to seconds
+
+        if notify.map(|notify| (idle + notify) >= time).unwrap_or(true) {
+            if notify.is_some() && !ran_notify {
+                invoke(&notifier.unwrap());
+                ran_notify = true;
+                delay = Duration::from_secs(1);
+                time_new = idle + notify.unwrap();
+            } else if idle >= time_new {
+                if !ran_timer {
+                    invoke(&timer);
+                    ran_timer = true;
+                    delay = default_delay;
+                }
+            }
+        } else {
+            if ran_notify && !ran_timer {
+                if let Some(canceller) = canceller {
+                    invoke(&canceller);
+                }
+            }
+            ran_timer  = false;
+            ran_notify = false;
+            delay = Duration::from_secs(SCALE);
+        }
+
+        thread::sleep(delay);
+    }
+}
+fn get_idle(display: *mut Display) -> Result<u64, ()> {
+    let info = unsafe { XScreenSaverAllocInfo() };
+    let _cleanup = DeferXFree(info as *mut c_void);
+
+    if unsafe { XScreenSaverQueryInfo(display, XDefaultRootWindow(display), info) } != 1 {
+        eprintln!("failed to query screen saver info");
+        return Err(());
+    }
+
+    Ok(unsafe { (*info).idle })
+}
+fn invoke(cmd: &str) {
+    if let Err(err) =
+        Command::new("bash")
+            .arg("-c")
+            .arg(cmd)
+            .output() {
+        eprintln!("failed to invoke command: {}", err);
+    }
+}
