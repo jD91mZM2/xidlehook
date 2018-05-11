fad1696cecc65cf0daff71b2fdb687ec8d0cbc74
jD91mZM2 <me@krake.one>
2018-05-11 05:34:42 +0200

    shell.nix: Don't override rustc

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 with import <nixpkgs> {};
 
-pkgs.xidlehook.overrideAttrs (orig: {
+pkgs.xidlehook.overrideAttrs (old: {
+  buildInputs = lib.remove pkgs.rustc old.buildInputs;
+
   LD_LIBRARY_PATH = "${pkgs.libpulseaudio}/lib";
 })
