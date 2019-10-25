6069d35a52b59e672d74c952744c6fcc9ca76945
jD91mZM2 <me@krake.one>
2019-10-25 10:08:26 +0200

    Delete old shell.nix

--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = with pkgs; [ xorg.libxcb xorg.libX11 xorg.libXScrnSaver libpulseaudio ];
-  nativeBuildInputs = with pkgs; [ pkg-config ];
-  LD_LIBRARY_PATH = "${pkgs.libpulseaudio}/lib";
-}
