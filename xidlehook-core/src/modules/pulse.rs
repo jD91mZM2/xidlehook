fb3e4739738d9fd6eb02695101339d109ba138cc
jD91mZM2 <me@krake.one>
2019-10-29 11:03:45 +0100

    Fix compilation and runtime of previous commit
    Also gets rid of panics, a library probably shouldn't panic
    unconditionally...

--- xidlehook-core/src/modules/pulse.rs
+++ xidlehook-core/src/modules/pulse.rs
@@ -3,7 +3,7 @@
 //! is used to implement `--not-when-audio` in the xidlehook example
 //! application.
 
-use crate::{Module, Progress, Result, TimerInfo};
+use crate::{Error, Module, Progress, Result, TimerInfo};
 
 use libpulse_binding::{
     callbacks::ListResult,
@@ -33,27 +33,31 @@ pub struct NotWhenAudio {
 impl NotWhenAudio {
     /// Connect to PulseAudio and subscribe to notification of changes
     pub fn new() -> Result<Self> {
-        let mut mainloop = Rc::new(RefCell::new(
-            Mainloop::new().expect("pulseaudio: failed to create main loop")
+        let mainloop = Rc::new(RefCell::new(
+            Mainloop::new().ok_or("pulseaudio: failed to create main loop")?
         ));
 
         let ctx = Rc::new(RefCell::new(
-            Context::new(&main, PA_NAME).expect("pulseaudio: failed to create context")
+            Context::new(&*mainloop.borrow(), PA_NAME).ok_or("pulseaudio: failed to create context")?
         ));
 
         // Setup context state change callback
         {
-            let ml_ref = Rc::clone(&mainloop);
+            let mainloop_ref = Rc::clone(&mainloop);
             let ctx_ref = Rc::clone(&ctx);
 
             ctx.borrow_mut().set_state_callback(Some(Box::new(move || {
+                // Unfortunately, we need to bypass the runtime borrow
+                // checker here of RefCell here, see
+                // https://github.com/jnqnfe/pulse-binding-rust/issues/19
+                // for details.
                 let state = unsafe { &*ctx_ref.as_ptr() } // Borrow checker workaround
                     .get_state();
                 match state {
                     context::State::Ready |
                     context::State::Failed |
                     context::State::Terminated => {
-                        unsafe { &mut *ml_ref.as_ptr() } // Borrow checker workaround
+                        unsafe { &mut *mainloop_ref.as_ptr() } // Borrow checker workaround
                             .signal(false);
                     },
                     _ => {},
@@ -62,26 +66,28 @@ impl NotWhenAudio {
         }
 
         ctx.borrow_mut().connect(None, context::flags::NOFLAGS, None)
-            .expect("pulseaudio: failed to connect context");
+            .map_err(|err| format!("pulseaudio: failed to connect context: {}", err))?;
 
         mainloop.borrow_mut().lock();
-        mainloop.borrow_mut().start().expect("pulseaudio: failed to start mainloop");
+        if let Err(err) = mainloop.borrow_mut().start() {
+            mainloop.borrow_mut().unlock();
+            return Err(Error::from(format!("pulseaudio: failed to start mainloop: {}", err)))?;
+        }
 
         // Wait for context to be ready
         loop {
             match ctx.borrow().get_state() {
-                pulse::context::State::Ready => { break; },
-                pulse::context::State::Failed |
-                pulse::context::State::Terminated => {
+                State::Ready => { break; },
+                State::Failed |
+                State::Terminated => {
                     mainloop.borrow_mut().unlock();
                     mainloop.borrow_mut().stop();
-                    panic!("pulseaudio: context state failed/terminated unexpectedly");
-                    return;
+                    return Err("pulseaudio: context state failed/terminated unexpectedly".into());
                 },
                 _ => { mainloop.borrow_mut().wait(); },
             }
         }
-        context.borrow_mut().set_state_callback(None);
+        ctx.borrow_mut().set_state_callback(None);
 
         let counter = Rc::new(Counter {
             in_progress: Cell::new(0),
@@ -116,12 +122,12 @@ impl NotWhenAudio {
         // to obtain a fresh count of active input sinks.
         {
             let ctx_ref = Rc::clone(&ctx);
-            let counter = Rc::clone(&counter);
-            let get_sinks_cl = Rc::Clone(&get_sinks);
+            let counter_ref = Rc::clone(&counter);
+            let get_sinks = get_sinks.clone();
 
             ctx.borrow_mut().set_subscribe_callback(Some(Box::new(move |_, _, _| {
                 let ctx_ref = unsafe { &mut *ctx_ref.as_ptr() }; // Borrow checker workaround
-                get_sinks_cl(ctx_ref, counter);
+                get_sinks(ctx_ref, Rc::clone(&counter_ref));
             })));
         }
 
@@ -129,7 +135,7 @@ impl NotWhenAudio {
         ctx.borrow_mut().subscribe(Facility::SinkInput.to_interest_mask(), |_| ());
 
         // Check if audio is already playing
-        get_sinks(ctx.borrow_mut(), Rc::clone(&counter));
+        get_sinks(&mut ctx.borrow_mut(), Rc::clone(&counter));
 
         mainloop.borrow_mut().unlock();
 
@@ -143,10 +149,12 @@ impl fmt::Debug for NotWhenAudio {
 }
 impl Drop for NotWhenAudio {
     fn drop(&mut self) {
-        self.mainloop.borrow_mut().lock();
+        debug!("Stopping PulseAudio main loop");
         self.mainloop.borrow_mut().stop();
+        self.mainloop.borrow_mut().lock();
         self.ctx.borrow_mut().disconnect();
         self.mainloop.borrow_mut().unlock();
+        debug!("Stopped");
     }
 }
 impl Module for NotWhenAudio {
