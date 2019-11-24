f297bc7c5d9465a0233cbb81b5b51a4febe411ac
jD91mZM2 <me@krake.one>
2019-11-24 10:33:51 +0100

    Rust 1.39 is now stable

--- test-daemon.sh
+++ test-daemon.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+# -*- eval: (aggressive-indent-mode -1); -*-
 
 set -e
 
-cargo +beta run -- \
-      --timer 2 "echo 2" "" \
-      --timer 1 "echo 3" "" \
-      --timer 1 "echo 4" "" \
-      --timer 1 "echo 5" "" \
-      --timer 5 "echo 10" "echo Cancelled between 10 and 15" \
-      --timer 5 "echo 15" "" \
-      --timer 5 "echo 20" "" \
-      --socket /tmp/xidlehook-test.sock \
-      --not-when-fullscreen --not-when-audio --once
+cargo run -- \
+    --timer 2 "echo 2" "" \
+    --timer 1 "echo 3" "" \
+    --timer 1 "echo 4" "" \
+    --timer 1 "echo 5" "" \
+    --timer 5 "echo 10" "echo Cancelled between 10 and 15" \
+    --timer 5 "echo 15" "" \
+    --timer 5 "echo 20" "" \
+    --socket /tmp/xidlehook-test.sock \
+    --not-when-fullscreen --not-when-audio --once
