5d04bcd3e5bfb517a9fa6b90a1a8327bb69c55c9
jD91mZM2 <me@krake.one>
2019-11-24 11:53:33 +0000

    Untested: Revised PulseAudio code
    See https://gitlab.com/jD91mZM2/xidlehook/issues/6#note_236958164.

--- xidlehook-core/src/modules/pulse.rs
+++ xidlehook-core/src/modules/pulse.rs
@@ -3,111 +3,144 @@
 //! is used to implement `--not-when-audio` in the xidlehook example
 //! application.
 
-use crate::{Module, Progress, Result, TimerInfo};
+use crate::{Error, Module, Progress, Result, TimerInfo};
 
 use libpulse_binding::{
     callbacks::ListResult,
-    context::{subscribe::Facility, Context, State},
+    context::{self, subscribe::Facility, Context, State},
     mainloop::threaded::Mainloop,
 };
 use log::debug;
 use std::{
-    cell::RefCell,
+    cell::{Cell, RefCell},
     fmt,
     rc::Rc,
-    sync::{
-        atomic::{AtomicUsize, Ordering},
-        Arc,
-    },
 };
 
 const PA_NAME: &str = "xidlehook";
 
 struct Counter {
-    in_progress: AtomicUsize,
-    last_total: AtomicUsize,
+    in_progress: Cell<usize>,
+    last_total: Cell<usize>,
 }
 
 /// See module-level docs
 pub struct NotWhenAudio {
-    counter: Arc<Counter>,
+    counter: Rc<Counter>,
     ctx: Rc<RefCell<Context>>,
-    main: Mainloop,
+    mainloop: Rc<RefCell<Mainloop>>,
 }
 impl NotWhenAudio {
-    /// Create a new pulseaudio main loop, but don't connect it yet
+    /// Connect to PulseAudio and subscribe to notification of changes
     pub fn new() -> Result<Self> {
-        let mut main = Mainloop::new().ok_or("pulseaudio: failed to create main loop")?;
-
-        // Should probably be thread-safe, see https://github.com/jnqnfe/pulse-binding-rust/issues/27
-        // ... but it can't be thread-safe, see https://github.com/jnqnfe/pulse-binding-rust/issues/19
-        let ctx = Rc::new(RefCell::new(
-            Context::new(&main, PA_NAME).ok_or("pulseaudio: failed to create context")?,
+        let mainloop = Rc::new(RefCell::new(
+            Mainloop::new().ok_or("pulseaudio: failed to create main loop")?
         ));
 
-        let counter = Arc::new(Counter {
-            in_progress: AtomicUsize::new(0),
-            last_total: AtomicUsize::new(0),
-        });
+        let ctx = Rc::new(RefCell::new(
+            Context::new(&*mainloop.borrow(), PA_NAME).ok_or("pulseaudio: failed to create context")?
+        ));
 
+        // Setup context state change callback
         {
-            let counter = Arc::clone(&counter);
-            let ctx = Rc::clone(&ctx);
+            let mainloop_ref = Rc::clone(&mainloop);
+            let ctx_ref = Rc::clone(&ctx);
 
-            Rc::clone(&ctx)
-                .borrow_mut()
-                .set_state_callback(Some(Box::new(move || {
-                    if ctx.borrow().get_state() != State::Ready {
-                        return;
-                    }
-
-                    let subscribe_callback = {
-                        let ctx = Rc::clone(&ctx);
-                        let counter = Arc::clone(&counter);
-
-                        move |_, _, _| {
-                            let counter = Arc::clone(&counter);
-                            ctx.borrow_mut()
-                                .introspect()
-                                .get_sink_input_info_list(move |res| match res {
-                                    ListResult::Item(item) => {
-                                        if !item.corked {
-                                            let count =
-                                                counter.in_progress.fetch_add(1, Ordering::SeqCst);
-                                            debug!("Partial count: {}", count);
-                                        }
-                                    },
-                                    ListResult::End | ListResult::Error => {
-                                        let count = counter.in_progress.swap(0, Ordering::SeqCst);
-                                        counter.last_total.store(count, Ordering::SeqCst);
-                                        debug!("Total sum: {}", count);
-                                    },
-                                });
-                        }
-                    };
-
-                    // Subscribe to sink input events
-                    {
-                        let mut ctx = ctx.borrow_mut();
-                        ctx.set_subscribe_callback(Some(Box::new(subscribe_callback.clone())));
-                        ctx.subscribe(Facility::SinkInput.to_interest_mask(), |_| ());
-                    }
-
-                    // In case audio already plays, trigger
-                    subscribe_callback(None, None, 0);
-                })));
+            ctx.borrow_mut().set_state_callback(Some(Box::new(move || {
+                // Unfortunately, we need to bypass the runtime borrow
+                // checker here of RefCell here, see
+                // https://github.com/jnqnfe/pulse-binding-rust/issues/19
+                // for details.
+                let state = unsafe { &*ctx_ref.as_ptr() } // Borrow checker workaround
+                    .get_state();
+                match state {
+                    context::State::Ready |
+                    context::State::Failed |
+                    context::State::Terminated => {
+                        unsafe { &mut *mainloop_ref.as_ptr() } // Borrow checker workaround
+                            .signal(false);
+                    },
+                    _ => {},
+                }
+            })));
         }
 
-        // We sadly can't use borrow_mut here because that keeps a
-        // mutable reference alive while it's runnig all the
-        // callbacks, leading to mutability errors there. See
-        // https://github.com/jnqnfe/pulse-binding-rust/issues/19.
-        unsafe { &mut *ctx.as_ptr() }
-            .connect(None, 0, None)
-            .map_err(|err| format!("pulseaudio: {}", err))?;
-        main.start().map_err(|err| format!("pulseaudio: {}", err))?;
+        ctx.borrow_mut().connect(None, context::flags::NOFLAGS, None)
+            .map_err(|err| format!("pulseaudio: failed to connect context: {}", err))?;
 
-        Ok(Self { counter, ctx, main })
+        mainloop.borrow_mut().lock();
+
+        if let Err(err) = mainloop.borrow_mut().start() {
+            mainloop.borrow_mut().unlock();
+            return Err(Error::from(format!("pulseaudio: failed to start mainloop: {}", err)))?;
+        }
+
+        // Wait for context to be ready
+        loop {
+            match ctx.borrow().get_state() {
+                State::Ready => { break; },
+                State::Failed |
+                State::Terminated => {
+                    mainloop.borrow_mut().unlock();
+                    mainloop.borrow_mut().stop();
+                    return Err("pulseaudio: context state failed/terminated unexpectedly".into());
+                },
+                _ => { mainloop.borrow_mut().wait(); },
+            }
+        }
+        ctx.borrow_mut().set_state_callback(None);
+
+        let counter = Rc::new(Counter {
+            in_progress: Cell::new(0),
+            last_total: Cell::new(0),
+        });
+
+        // Closure for setting up async count of input sinks
+        let get_sinks = |ctx: &mut Context, counter: Rc<Counter>| {
+            ctx
+                .introspect()
+                .get_sink_input_info_list(move |res|
+                    match res {
+                        ListResult::Item(item) => {
+                            if !item.corked {
+                                let count = counter.in_progress.get() + 1;
+                                counter.in_progress.set(count);
+                                debug!("Partial count: {}", count);
+                            }
+                        },
+                        ListResult::End | ListResult::Error => {
+                            let count = counter.in_progress.replace(0);
+                            counter.last_total.set(count);
+                            debug!("Total sum: {}", count);
+                        },
+                    }
+                );
+        };
+
+        // Setup notification callback
+        //
+        // Upon notification of a change, we will make use of introspection
+        // to obtain a fresh count of active input sinks.
+        {
+            let ctx_ref = Rc::clone(&ctx);
+            let counter_ref = Rc::clone(&counter);
+            let get_sinks = get_sinks.clone();
+
+            ctx.borrow_mut().set_subscribe_callback(Some(Box::new(move |_, _, _| {
+                let ctx_ref = unsafe { &mut *ctx_ref.as_ptr() }; // Borrow checker workaround
+                get_sinks(ctx_ref, Rc::clone(&counter_ref));
+            })));
+        }
+
+        // Subscribe to sink input events
+        ctx.borrow_mut().subscribe(Facility::SinkInput.to_interest_mask(), |_| ());
+
+        // Check if audio is already playing
+        get_sinks(&mut ctx.borrow_mut(), Rc::clone(&counter));
+
+        mainloop.borrow_mut().unlock();
+
+        Ok(Self { counter, ctx, mainloop })
     }
 }
 impl fmt::Debug for NotWhenAudio {
@@ -117,14 +150,19 @@ impl fmt::Debug for NotWhenAudio {
 }
 impl Drop for NotWhenAudio {
     fn drop(&mut self) {
-        // See note above
-        unsafe { &mut *self.ctx.as_ptr() }.disconnect();
-        self.main.stop();
+        debug!("Stopping PulseAudio main loop");
+        self.mainloop.borrow_mut().stop();
+        self.mainloop.borrow_mut().lock();
+        self.ctx.borrow_mut().disconnect();
+        self.mainloop.borrow_mut().unlock();
+        debug!("Stopped");
     }
 }
 impl Module for NotWhenAudio {
     fn pre_timer(&mut self, _timer: TimerInfo) -> Result<Progress> {
-        let players = self.counter.last_total.load(Ordering::SeqCst);
+        self.mainloop.borrow_mut().lock();
+        let players = self.counter.last_total.get();
+        self.mainloop.borrow_mut().unlock();
         if players == 0 {
             Ok(Progress::Continue)
         } else {
