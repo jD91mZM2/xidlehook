9e29e9a63c235059b2ea2c78f3f0671581dd269a
jD91mZM2 <me@krake.one>
2020-03-11 17:30:35 +0100

    Bump version

--- publish.sh
+++ publish.sh
@@ -3,12 +3,6 @@
 
 set -e
 
-if ! which cargo-release 2>&1 > /dev/null; then
-    echo "You need to install cargo-release"
-    echo "$ cargo install cargo-release"
-    exit 1
-fi
-
 pandoc -s --toc -t gfm README.org -o README.md
 eval "$VISUAL" README.md
 
@@ -19,19 +13,23 @@ echo "Running tests..."
 cargo check
 cargo test
 
-echo "Making sure packaging works..."
-cargo publish --dry-run --manifest-path xidlehook-core/Cargo.toml
-cargo publish --dry-run --manifest-path xidlehook-daemon/Cargo.toml
+do_package() {
+    echo "Making sure packaging works..."
+    cargo publish --dry-run --manifest-path "$1"/Cargo.toml
 
-version="$(sed -e 's/^version\s*=\s*"\([0-9]\+\.[0-9]\+\.[0-9]\+\)"/\1/' -e t -e d xidlehook-daemon/Cargo.toml)"
+    version="$(sed -e 's/^version\s*=\s*"\([0-9]\+\.[0-9]\+\.[0-9]\+\)"/\1/' -e t -e d "$1"/Cargo.toml | head -n1)"
 
-git status
+    git status
 
-echo "Publishing version $version!!! Press enter to continue."
-read -r
+    echo "Publishing version $version of $1!!! Press enter to continue."
+    read -r
 
+    cargo publish --manifest-path "$1"/Cargo.toml
+}
+
+do_package xidlehook-core
+echo "Waiting for crates.io to update"
+sleep 5
+
+do_package xidlehook-daemon # sets "$version"
 git tag "$version" --annotate --sign -m "xidlehook $version"
-
-cargo publish --manifest-path xidlehook-core/Cargo.toml &
-cargo publish --manifest-path xidlehook-daemon/Cargo.toml &
-wait
