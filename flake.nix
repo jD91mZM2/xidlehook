8c419d40c8e126c5d81eff0af5c8dfd98a0542a6
jD91mZM2 <me@krake.one>
2021-01-30 15:22:48 +0100

    Fix nix derivation

--- flake.nix
+++ flake.nix
@@ -3,38 +3,42 @@
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
+
+    # --- SEE https://github.com/nmattia/naersk/pull/141 ---
+    # naersk.url = "github:nmattia/naersk";
+    naersk.url = "github:jD91mZM2/naersk/flake-ws";
   };
 
   outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages."${system}";
-      naersk-lib = naersk.lib."${system}";
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
 
-      nativeBuildInputs = with pkgs; [ pkgconfig python3 ];
-      buildInputs = with pkgs; [ libpulseaudio xorg.libxcb xorg.libXScrnSaver x11 ];
-    in rec {
-      # `nix build`
-      packages.xidlehook = naersk-lib.buildPackage {
-        pname = "xidlehook";
-        # TODO: Use workspaces
-        # src = ./.;
-        src = ./xidlehook-daemon;
+        nativeBuildInputs = with pkgs; [ pkgconfig python3 ];
+        buildInputs = with pkgs; [ libpulseaudio xorg.libxcb xorg.libXScrnSaver x11 ];
+      in
+      rec {
+        # `nix build`
+        packages.xidlehook = naersk-lib.buildPackage {
+          pname = "xidlehook";
+          # TODO: Use workspaces
+          src = ./.;
 
-        inherit nativeBuildInputs buildInputs;
-      };
-      defaultPackage = packages.xidlehook;
+          inherit nativeBuildInputs buildInputs;
+        };
+        defaultPackage = packages.xidlehook;
 
-      # `nix run`
-      apps.xidlehook = utils.lib.mkApp {
-        drv = packages.xidlehook;
-      };
-      defaultApp = apps.xidlehook;
+        # `nix run`
+        apps.xidlehook = utils.lib.mkApp {
+          drv = packages.xidlehook;
+        };
+        defaultApp = apps.xidlehook;
 
-      # `nix develop`
-      devShell = pkgs.mkShell {
-        buildInputs = buildInputs;
-        nativeBuildInputs = nativeBuildInputs ++ [ pkgs.rustc pkgs.cargo ];
-      };
-    });
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          buildInputs = buildInputs;
+          nativeBuildInputs = nativeBuildInputs ++ [ pkgs.rustc pkgs.cargo ];
+        };
+      });
 }
